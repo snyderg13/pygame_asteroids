f511c74915d2bfc7c1a8453030f9bd86bbf455aa
snyderg13 <georgesny13@gmail.com>
2025-06-08 12:41:16 -0400

    initial game revision with player, groups, asteroids and movement functioning

---
+++ main.py
@@ -0,0 +1,43 @@
+import pygame
+from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
+
+def main():
+    print("Starting Asteroids!")
+    print(f"Screen width: {SCREEN_WIDTH}")
+    print(f"Screen height: {SCREEN_HEIGHT}")
+
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    game_clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+    ast_field = AsteroidField()
+    dt = 0
+
+
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+
+        updatable.update(dt)
+
+        screen.fill("black")
+
+        for sprite in drawable:
+            sprite.draw(screen)
+
+        pygame.display.flip()
+        dt = game_clock.tick(60) / 1000
+
+if __name__ == "__main__":
+    main()
